05bd201e94a05f59f9fb35ea1f3c7eb8eff21950
maoge <sun409377708@163.com>
2017-01-06 13:54:01 +0800

    创建项目, 修改ignore文件

---
+++ RunningMan/RunningMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunningMan
+//
+//  Created by maoge on 17/1/6.
+//  Copyright © 2017年 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunningMan/RunningMan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunningMan
+//
+//  Created by maoge on 17/1/6.
+//  Copyright © 2017年 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
